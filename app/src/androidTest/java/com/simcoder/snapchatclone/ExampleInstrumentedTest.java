b5ca196552fcf9e60e0b649b4800a9b83df0f3d9
JaybabyFrom69th <79046315+JaybabyFrom69th@users.noreply.github.com>
2021-02-15 07:01:51 -0500

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/simcoder/snapchatclone/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/simcoder/snapchatclone/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.simcoder.snapchatclone;
+package com.simcoder.snapchatclone
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.simcoder.snapchatclone", appContext.getPackageName());
+        assertEquals("com.simcoder.snapchatclone", appContext.getjj_versace());
     }
 }
